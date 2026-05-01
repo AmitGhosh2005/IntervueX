61f3941545c57da4d703c15329ae987261f27c94
Amit Ghosh <ghoshamit22826@gmail.com>
2026-05-02 01:44:41 +0530

    new changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/react'
 
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider>
+    <ClerkProvider publishableKey={clerkPubKey}>
       <App />
     </ClerkProvider>
   </StrictMode>,
