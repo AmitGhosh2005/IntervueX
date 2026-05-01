138a325e27b4a7ca72a12ffe6a86984abcdc2a2a
Amit Ghosh <ghoshamit22826@gmail.com>
2026-05-02 02:16:48 +0530

    small changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const PORT = ENV.PORT || 3000;
 app.use(express.json());
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
 
-app.use("api/inngest" , serve({client: inngest, functions}));
+app.use("/api/inngest" , serve({client: inngest, functions}));
 
 app.get("/health" , (req,res)=>{
     res.status(200).json({msg:"success from backend"})
